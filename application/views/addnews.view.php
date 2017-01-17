bba3cb41c3019b75d212ac6db9d41d344cffbd01
willy <willyjunir91@gmail.com>
2017-01-17 22:35:37 +0100

    redacteur

--- application/views/addnews.view.php
+++ application/views/addnews.view.php
@@ -5,3 +5,14 @@
  * Date: 15/01/17
  * Time: 02:34
  */
+defined('3INEWS') or die('Acces Denied');
+$this->addStyleSheets('css/accueil.css');
+$this->setPageTitle('Nouvelle news');
+$this->addForm->render();
+
+$image = addslashes(file_get_contents($_FILES['images']['tmp_name']));
+$this->imaage =$image;
+
+?>
+<pre><?php print_r($_POST)?>/pre>
+
